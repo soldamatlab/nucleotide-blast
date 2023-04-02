a1368251a3f5e63ccb3f38bd9e4190ccbdd86028
soldamat <soldamat@fel.cvut.cz>
2023-04-02 19:11:41 +0200

    add additional info prints

--- lib/blast/blast.py
+++ lib/blast/blast.py
@@ -2,6 +2,7 @@ from lib.blast.seeds import get_seeds
 from lib.blast.hits import find_hits_in_database
 from lib.blast.extension import extend_hits
 
+
 def blast(sequence, database, k, t, S):
     """
     :param sequence:
@@ -14,11 +15,18 @@ def blast(sequence, database, k, t, S):
                         of shape (seqeunce_range, file_range)
     """
     print("BLAST started.")
+
     print("Searching for seeds in query sequence.")
     seeds, seed_positions = get_seeds(sequence, k, t, S)
+    print("%d unique seeds found with %d viable \"seed & position in query sequence\" combinations." %
+          (len(seeds), sum([len(positions) for positions in seed_positions])))
+
     print("Searching for seed hits in database.")
     hits = find_hits_in_database(seeds, database, k)
+    print("%d total hits found." % sum([len(file_hits) for file_hits in hits]))
+
     print("Performing gapless extension of the hits.")
     matches = extend_hits(database, hits, k, seed_positions, sequence, S)
+
     print("BLAST finished.")
     return matches
